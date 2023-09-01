a7be48296b46b13800d73416cca7ec0e5d60feed
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-09-01 19:08:55 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 河野保 on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
